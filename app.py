129b87f543da7bd7358c0f8c488352b85ae3a5af
ssjss77 <43239391+ssjss77@users.noreply.github.com>
2020-09-05 23:06:12 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,39 +1,28 @@
-from flask import Flask, request, url_for, redirect, render_template, jsonify
-
+from flask import Flask,request, url_for, redirect, render_template, jsonify
 from pycaret.regression import *
-
 import pandas as pd
-
 import pickle
 import numpy as np
 
-app = Flask( __name__ )
+app = Flask(__name__)
 
 model = load_model( 'model_2020_09_02' )
 cols = ['age', 'sex', 'bmi', 'children', 'smoker', 'region']
 
-
-@app.route( '/' )
+@app.route('/')
 def home():
-    return render_template( "home.html" )
+    return render_template("home.html")
 
-
-
-@app.route( '/predict', methods=['POST'] )
-
+@app.route('/predict',methods=['POST'])
 def predict():
     int_features = [x for x in request.form.values()]
-    final = np.array( int_features )
-
-    data_unseen = pd.DataFrame( [final], columns=cols )
-    prediction = predict_model( model, data=data_unseen, round=0 )
-    prediction = int( prediction.Label[0] )
-    
-    return render_template( 'home.html', pred='La facture attendue sera {} €'.format( prediction ) )
-
+    final = np.array(int_features)
+    data_unseen = pd.DataFrame([final], columns = cols)
+    prediction = predict_model(model, data=data_unseen, round = 0)
+    prediction = int(prediction.Label[0])
+    return render_template('home.html',pred='Expected Bill will be {}'.format(prediction))
 
 @app.route('/predict_api',methods=['POST'])
-
 def predict_api():
     data = request.get_json(force=True)
     data_unseen = pd.DataFrame([data])
@@ -43,4 +32,3 @@ def predict_api():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
